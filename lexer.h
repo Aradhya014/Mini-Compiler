b67408a622e75692e9dd85c0ed1eaff9adbfb021
Aradhya Rastogi <142193262+Aradhya014@users.noreply.github.com>
2026-04-22 21:44:42 +0530

    Update lexer.h

--- lexer.h
+++ lexer.h
@@ -1,8 +1,62 @@
+// #ifndef LEXER_H
+// #define LEXER_H
+
+// #include <stdio.h>
+
+// typedef enum {
+//     TOKEN_INT,
+//     TOKEN_FLOAT,        
+//     TOKEN_IF,
+//     TOKEN_ELSE,
+//     TOKEN_WHILE,
+//     TOKEN_RETURN,
+//     TOKEN_PRINT,
+//     TOKEN_MAIN,
+
+//     TOKEN_IDENTIFIER,
+//     TOKEN_INT_LITERAL,      
+//     TOKEN_FLOAT_LITERAL,    
+
+//     TOKEN_PLUS,
+//     TOKEN_MINUS,
+//     TOKEN_MUL,
+//     TOKEN_DIV,
+
+//     TOKEN_ASSIGN,
+//     TOKEN_EQ,
+//     TOKEN_NEQ,
+//     TOKEN_LT,
+//     TOKEN_GT,
+//     TOKEN_LE,
+//     TOKEN_GE,
+
+//     TOKEN_LPAREN,
+//     TOKEN_RPAREN,
+//     TOKEN_LBRACE,
+//     TOKEN_RBRACE,
+//     TOKEN_SEMICOLON,
+//     TOKEN_COMMA,
+
+//     TOKEN_EOF,
+//     TOKEN_UNKNOWN
+// } TokenType;
+
+// typedef struct {
+//     TokenType type;
+//     char lexeme[100];
+//     int line;
+// } Token;
+
+// Token getNextToken(FILE *fp);
+// const char* tokenTypeToString(TokenType type);
+
+// #endif
+
 #ifndef LEXER_H
 #define LEXER_H
-
+ 
 #include <stdio.h>
-
+ 
 typedef enum {
     TOKEN_INT,
     TOKEN_FLOAT,        
@@ -12,16 +66,16 @@ typedef enum {
     TOKEN_RETURN,
     TOKEN_PRINT,
     TOKEN_MAIN,
-
+ 
     TOKEN_IDENTIFIER,
     TOKEN_INT_LITERAL,      
     TOKEN_FLOAT_LITERAL,    
-
+ 
     TOKEN_PLUS,
     TOKEN_MINUS,
     TOKEN_MUL,
     TOKEN_DIV,
-
+ 
     TOKEN_ASSIGN,
     TOKEN_EQ,
     TOKEN_NEQ,
@@ -29,25 +83,30 @@ typedef enum {
     TOKEN_GT,
     TOKEN_LE,
     TOKEN_GE,
-
+ 
     TOKEN_LPAREN,
     TOKEN_RPAREN,
     TOKEN_LBRACE,
     TOKEN_RBRACE,
     TOKEN_SEMICOLON,
     TOKEN_COMMA,
-
+ 
     TOKEN_EOF,
-    TOKEN_UNKNOWN
+    TOKEN_UNKNOWN,
+    TOKEN_INVALID   
 } TokenType;
-
+ 
 typedef struct {
     TokenType type;
     char lexeme[100];
     int line;
 } Token;
-
+ 
 Token getNextToken(FILE *fp);
 const char* tokenTypeToString(TokenType type);
+ 
 
+extern int lexErrorCount;
+ 
 #endif
+ 
